6aee2251cee5cd335114294f2b58085deb55d015
Philipp Anders <philipp.anders.pa@gmail.com>
2015-03-15 20:28:29 +0100

    init commit, page 34

--- app/router.js
+++ app/router.js
@@ -6,6 +6,10 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('friends', function() {
+    this.route('new');
+    this.route('show', {path: ':friend_id'});
+  });
 });
 
 export default Router;
